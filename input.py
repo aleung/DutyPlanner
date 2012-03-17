ed7d7268c1e82f50d7517ae658d48754c149706e
Leo Liang <leoliang@gmail.com>
2012-03-17 18:24:18 +0800

    Make all possible schedule by permutation. Unusable because the result is huge.

--- input.py
+++ input.py
@@ -3,22 +3,39 @@
 import datetime
 
 
-iteration_begin_date = datetime.datetime(2012, 3, 19)
+iteration_begin_date = datetime.datetime(2012, 4, 2)
 
 # days in each iterations
-iteration_days = [7, 7, 7, 7, 7]
+iteration_days = [7, 7, 7, 7]
 
 # exclude from schedule: (iteration_id, staff_name)
 schedule_exclude = [
-	(0, "GordonLi"),
+	(0, "YanGao"),
+	(0, "JinLiangLi"),
+	(0, "KennyHuang"),
 	(0, "TomZhou"),
-	(1, "TsaiSong"),
+	(0, "DavidNong"),
+	(0, "GordonLi"),
+	(0, "JessicaXie"),
+	(0, "TsaiSong"),
+	(1, "KennyHuang"),
+	(1, "TomZhou"),
+	(1, "DavidNong"),
+	(2, "KennyHuang"),
+	(2, "TomZhou"),
+	(2, "DavidNong"),
+	(2, "YanGao"),
+	(3, "KennyHuang"),
+	(3, "TomZhou"),
+	(3, "DavidNong"),
 ]
+
+max_continuous_iterations = 2
 
 group_definition = {
 	'competence_requirement' : {
-		'MSC' : 0.85 ,
-		'NGW' : 0.85 ,	
+		'MSC' : 0.75 ,
+		'NGW' : 0.75 ,	
 	},
 	'roles' : [
 		('Major', 2),
@@ -29,62 +46,62 @@ group_definition = {
 staffs = {
 	'TsaiSong' : {
 		'competence' : {
-			'MSC' : 0.9 ,
-			'NGW' : 0.4 ,
+			'MSC' : 0.85 ,
+			'NGW' : 0.85 ,
 		},
 	},
 	'GordonLi' : {
 		'competence' : {
-			'MSC' : 0.3 ,
-			'NGW' : 0.6 ,
+			'MSC' : 0.85 ,
+			'NGW' : 0.85 ,
 		},
 	},
 	'YanGao' : {
 		'competence' : {
-			'MSC' : 0.5 ,
-			'NGW' : 0.8 ,
+			'MSC' : 0.75 ,
+			'NGW' : 0.1 ,
 		},
 	},
 	'TomZhou' : {
 		'competence' : {
-			'MSC' : 0.9 ,
-			'NGW' : 0.9 ,
+			'MSC' : 0.85 ,
+			'NGW' : 0.75 ,
 		},
 	},
 	'JinLiangLi' : {
 		'competence' : {
-			'MSC' : 0.9 ,
-			'NGW' : 0.9 ,
+			'MSC' : 0.7 ,
+			'NGW' : 0.7 ,
 		},
 	},
-	# 'RyanWeng' : {
-	# 	'competence' : {
-	# 		'MSC' : 0.9 ,
-	# 		'NGW' : 0.9 ,
-	# 	},
-	# },
+	'RyanWeng' : {
+		'competence' : {
+			'MSC' : 0.7 ,
+			'NGW' : 0.7 ,
+		},
+	},
 	'KennyHuang' : {
 		'competence' : {
-			'MSC' : 0.9 ,
-			'NGW' : 0.9 ,
+			'MSC' : 0.65 ,
+			'NGW' : 0.65,
 		},
 	},
 	'KacyLiu' : {
 		'competence' : {
-			'MSC' : 0.9 ,
-			'NGW' : 0.9 ,
+			'MSC' : 0.7 ,
+			'NGW' : 0.7 ,
 		},
 	},
 	'DavidNong' : {
 		'competence' : {
-			'MSC' : 0.9 ,
-			'NGW' : 0.9 ,
+			'MSC' : 0.6 ,
+			'NGW' : 0.6 ,
 		},
 	},
 	'JessicaXie' : {
 		'competence' : {
-			'MSC' : 0.9 ,
-			'NGW' : 0.9 ,
+			'MSC' : 0.1 ,
+			'NGW' : 0.7 ,
 		},
 	},
 }
--- schedule.py
+++ schedule.py
@@ -3,10 +3,11 @@
 import itertools
 import input
 
+
 class Group:
 	def __init__(self, staffs):
 		self.staffs = staffs
-		# {staff_name, role}
+		# {staff_name : role}
 		self.members = {}
 
 	def set_member(self, role, staff):
@@ -20,6 +21,7 @@ class Group:
 	def get_competence(self, area):
 		competences = [self.staffs[name]['competence'][area] for name in self.members.keys()]
 		return max(competences)
+
 
 class GroupBuilder():
 	def __init__(self, staffs, group_definition):
@@ -59,5 +61,49 @@ class GroupBuilder():
 		return True
 
 
+class ScheduleBuilder():
+	def __init__(self, groups, number_of_iterations, schedule_exclude, max_continuous_iterations):
+		self.groups = groups
+		self.number_of_iterations = number_of_iterations
+		self.schedule_exclude = schedule_exclude
+		self.max_continuous_iterations = max_continuous_iterations
+		self.schedules = []
+
+	def build(self):
+		for schedule in itertools.permutations(self.groups, self.number_of_iterations):
+			if self.validate_schedule(schedule):
+				self.schedules.append(schedule)
+		return self.schedules
+
+	def validate_schedule(self, schedule):
+		for i in range(self.number_of_iterations):
+			exclude_staffs = set([ name for (iteration, name) in self.schedule_exclude if iteration == i ])
+			if exclude_staffs.intersection(schedule[i].get_members().keys()):
+				return False
+			if i >= self.max_continuous_iterations:
+				for member in schedule[i].get_members().keys():					
+					for prev in range(self.max_continuous_iterations):
+						if member in schedule[i-prev].get_members().keys:
+							return False
+		return True
+
+
+class SchedulePrinter():
+	def __init__(self):
+		pass
+
+	def print_schedule(self, schedule):
+		for num in range(len(schedule)):
+			print "Iteration " + num
+			print schedule[num].get_members
+		pass #TODO
+
+
 if __name__ == "__main__":
+	groups = GroupBuilder(input.staffs, input.group_definition).build()
+	print "%d group options available." % len(groups)
+	schedules = ScheduleBuilder(groups, len(input.iteration_days), input.schedule_exclude, input.max_continuous_iterations).build()
+	printer = SchedulePrinter()
+	for schedule in schedules:
+		printer.print_schedule(schedule)
 	pass
